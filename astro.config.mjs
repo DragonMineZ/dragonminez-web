9a11468cd2942028db1f3fef71b9166e05d32954
bennndev <benjamin.sullca1103@gmail.com>
2026-02-14 15:26:05 -0500

    feat(auth): auth with Clerk and Discord provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+// Clerk
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { dark } from "@clerk/themes";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk({
+      appearance: {
+        theme: dark,
+      },
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
